26f5799600f63a3fa5b76b10b64a22c3bfbe856d
Saskia <saskia@pc>
2011-04-30 17:22:48 +0200

    makeKey, readKey und encipher begonnen

--- 02_RunningKey/src/task2/RunningKey.java
+++ 02_RunningKey/src/task2/RunningKey.java
@@ -14,8 +14,12 @@ package task2;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import de.tubs.cs.iti.jcrypt.chiffre.CharacterMapping;
 import de.tubs.cs.iti.jcrypt.chiffre.Cipher;
@@ -29,6 +33,9 @@ import de.tubs.cs.iti.jcrypt.chiffre.Cipher;
 public class RunningKey extends Cipher {
 	
 	final boolean DEBUG = true;
+	
+	int keyAlphLenght;
+	String keyFilePath;
 
   /**
    * Analysiert den durch den Reader <code>ciphertext</code> gegebenen
@@ -56,6 +63,7 @@ public class RunningKey extends Cipher {
    * Der Writer, der den Klartext schreiben soll.
    */
   public void decipher(BufferedReader ciphertext, BufferedWriter cleartext) {
+	  if(DEBUG) System.out.println(">>>decipher called");
 
   }
 
@@ -70,7 +78,20 @@ public class RunningKey extends Cipher {
    * Der Writer, der den Chiffretext schreiben soll.
    */
   public void encipher(BufferedReader cleartext, BufferedWriter ciphertext) {
-
+	  if(DEBUG) System.out.println(">>>encipher called");
+	  String msg = "";
+	  
+	  //Lese die Buchstaben des Keys ein
+	  ArrayList<Integer> keyChars,clearChars;
+	  keyChars = readFileToList(keyFilePath);
+	  clearChars = readBufferedReaderToList(cleartext);
+	  
+	  if(keyChars.size() >= clearChars.size()) {
+		  doEncipher(keyChars,clearChars);
+	  } else {
+		  msg = "Schlüsseldatei ist zu klein! Verschlüsseln wird abgebrochen.";
+		  System.out.println(msg);
+	  }
   }
 
   /**
@@ -153,9 +174,6 @@ public class RunningKey extends Cipher {
 			e.printStackTrace();
 		}   
     } 
-    
-//    //Setze globalen Moduls (Alphabetlänge)
-//    modulus = alphabetLength;
   }
 
   /**
@@ -167,7 +185,28 @@ public class RunningKey extends Cipher {
    * @see #writeKey writeKey
    */
   public void readKey(BufferedReader key) {
-
+	  if(DEBUG) System.out.println(">>>readKey called");
+	  String s;
+	  String[] sKey;
+	  
+	  try {
+			s = key.readLine();
+			sKey = s.split(" ", 2);
+			if(DEBUG) { 
+				System.out.print(">>>> Eingelesen: " + s);
+				System.out.print("\t Array: " + Arrays.toString(sKey));
+				System.out.println();
+			}
+			if(sKey.length == 2) {
+				keyAlphLenght = Integer.parseInt(sKey[0]);
+				modulus = keyAlphLenght;
+				keyFilePath = sKey[1];
+			} else {
+				System.out.println("!ACHTUNG! Key wurde falsch eingelesen.");
+			}
+	  } catch (IOException e) {
+			e.printStackTrace();
+		}
   }
 
   /**
@@ -179,9 +218,13 @@ public class RunningKey extends Cipher {
    * @see #readKey readKey
    */
   public void writeKey(BufferedWriter key) {
+	  if(DEBUG) System.out.println(">>>writeKey called");
 
+	  
   }
-  
+  /**
+   * Für Tests
+   */
   private void testMethod() {
 	  System.out.println(">>>testMethod called");
 	
@@ -190,4 +233,55 @@ public class RunningKey extends Cipher {
 	  System.out.println(">>>/testMethod finished");
 	  System.exit(0);
   }
+  /**
+   * Liest aus einer Datei die einzelnen Zeichen aus und gibt die Zeichen als Liste zurück.
+   * @param filePath	Pfad zur auszulesenden Datei
+   * @return Liste der einzelnen Zeichen, Zahlenwerte der Zeichen
+   */
+  private ArrayList<Integer> readFileToList(String filePath) {
+	  ArrayList<Integer> back = new ArrayList<Integer>();
+  
+	  try {
+		  BufferedReader br = new BufferedReader(new FileReader(filePath));
+		  back = readBufferedReaderToList(br);
+	  } catch (FileNotFoundException e) {
+		  e.printStackTrace();
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
+	  
+	  return back;
+  }
+  
+  /**
+   * Liest aus einer Datei die einzelnen Zeichen aus und gibt die Zeichen als Liste zurück.
+   * @param reader	BufferedReader mit der auszulesenden Datei
+   * @return Liste der einzelnen Zeichen, Zahlenwerte der Zeichen
+   */
+  private ArrayList<Integer> readBufferedReaderToList(BufferedReader reader) {
+	  ArrayList<Integer> back = new ArrayList<Integer>();
+	  
+	  try {
+		  int character;
+		  while(reader.ready()) {
+			  character = reader.read();
+			  back.add(character);
+		  }
+		  reader.close();
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
+	  
+	  return back;
+  }
+  	
+  	/** 
+  	 * Methode zur eigentlichen Verschlüsselung
+  	 * @param keyChars	Liste der einzelnen Zeichen der Schlüsseldatei
+  	 * @param clearChars	Liste der einzelnen Zeichen der Klartextdatei
+  	 */
+	private void doEncipher(ArrayList<Integer> keyChars, ArrayList<Integer> clearChars) {
+		if(DEBUG) System.out.println(">>>doEncipher called");
+		
+	}
 }
