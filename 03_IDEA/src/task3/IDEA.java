1ccf0cd53e17e9b3375ff35fa7dff559a1a03670
Saskia <saskia@pc>
2011-05-06 11:27:40 +0200

    IDEA-Verschlüsselungsalgorithmus umgesetzt

--- 03_IDEA/src/task3/IDEA.java
+++ 03_IDEA/src/task3/IDEA.java
@@ -156,43 +156,43 @@ private short[] stringKeytoShortKey(String originalKey) {
    * @param key
    * @return
    */
-  private short calcBitwiseXor(short message, short key) {
+  private short calcBitwiseXor(short message1, short message2) {
 	  short back;
-	  back = (short) (message ^ key);
+	  back = (short) (message1 ^ message2);
 	  
 	  return back;
   }
   
   /**
    * Addition mod 2^16	2x 16bit eingaben, 1x 16 bit ausgabe
-   * @param message
-   * @param key
+   * @param message1
+   * @param message2
    * @return
    */
-  private short calcAdditionMod216(short message, short key) {
+  private short calcAdditionMod216(short message1, short message2) {
 	  short back;
-	  back = (short) ((message + key) % Math.pow(2, 16));
+	  back = (short) ((message1 + message2) % Math.pow(2, 16));
 	  
 	  return back;
   }
   
   /**
    * Multiplikation in Z*_((2^16)+1)	2x 16bit eingaben, 1x 16 bit ausgabe
-   * @param message
-   * @param key
+   * @param message1
+   * @param message2
    * @return
    */
-  private short calcMultiplikationZ(short message, short key) {
+  private short calcMultiplikationZ(short message1, short message2) {
 	  short back;
 	  int b;
-	  int m = message;
-	  int k = key;
+	  int m = message1;
+	  int k = message2;
 	  //Sonderfälle, wenn 0 eingegeben wird, ersetze durch 2^16
 	  if (m==0) m=(int) Math.pow(2,16);
 	  if (k==0) k=(int) Math.pow(2, 16);
 	  //eigentliche Rechnung
 	  int mod = (int) (Math.pow(2, 16) +1);
-	  b = ((message * key) % mod);
+	  b = ((message1 * message2) % mod);
 	  //Sonderfall, wenn 2^16 heraus kommt, ersetze durch 0
 	  if (b==Math.pow(2, 16)) b=0;
 	  back= (short) b;
@@ -214,6 +214,68 @@ private short[] stringKeytoShortKey(String originalKey) {
 	  for (int i=0;i<back.length;i++)
 		  back[i]=calcBitwiseXor(message[i],key[i]);
 	  return back;
+  }
+  
+  /**
+   * Umsetzung des IDEA
+   */
+  private short[] doIDEA(short[] m,short[][] k) {
+	  short[][] vK = new short[9][6]; //Temporär, Parameterübergabe
+	  short[][] vT = new short[5][4]; //Zwischenwerte des Algorithmus'
+	  short[] vZ = new short[4]; //Zwischenwerte der Nachrichten
+	  short[] vC = new short[4]; //Rückgabe
+	  
+	  //Prüfe Eingaben
+	  if(m.length != 4 || k.length != 9) {
+		  return null;
+	  }
+	  for (int i = 0; i < k.length; i++) {
+		if(k[i].length != vK[i].length) {
+			return null;
+		}
+	  }
+	  vK = k;
+	  
+	  //Füge Nachricht als Startwerte der Zwischennachrichten ein.
+	  for(int i = 0; i < vZ.length;i++) {
+		  vZ[i] = m[i];
+	  }
+	  
+	  //Runde r 1 bis 8
+	  for(int r = 0; r < 8; r++) {
+		  vT[1][1] = calcMultiplikationZ(vZ[1], vK[r][1]);	//Z1 MultZ K1[r]  	> T11
+		  vT[1][2] = calcAdditionMod216(vZ[2], vK[r][2]);	//Z2 Add216 K2[r] 	> T12
+		  vT[1][3] = calcAdditionMod216(vZ[3], vK[r][3]);	//Z3 Add216 K3[r] 	> T13
+		  vT[1][4] = calcMultiplikationZ(vZ[4], vK[r][4]);	//Z4 MultZ K4[r]  	> T14
+		  
+		  vT[2][1] = calcBitwiseXor(vT[1][1], vT[1][3]);	//T11 XOR T13 	> T21
+		  vT[2][2] = calcBitwiseXor(vT[1][2], vT[1][4]);	//T12 XOR T14 	> T22
+		  
+		  vT[3][1] = calcMultiplikationZ(vT[2][1], vK[r][5]);	//T21 MultZ K5[r] 	> T31
+		  vT[3][2] = calcAdditionMod216(vT[3][1], vT[2][2]);	//T31 Add216 T22  	> T32
+		  vT[3][3] = calcMultiplikationZ(vT[3][2], vK[r][6]);	//T32 MultZ K6[r] 	> T33
+		  vT[3][4] = calcAdditionMod216(vT[3][1], vT[3][4]);	//T31 Add216 T34  	> T34
+		  
+		  vT[4][1] = calcBitwiseXor(vT[1][1], vT[3][3]); //T11 XOR T33	> T41
+		  vT[4][2] = calcBitwiseXor(vT[1][3], vT[3][3]); //T13 XOR T33	> T42
+		  vT[4][3] = calcBitwiseXor(vT[1][2], vT[3][4]); //T12 XOR T34	> T43
+		  vT[4][4] = calcBitwiseXor(vT[1][4], vT[3][4]); //T14 XOR T34	> T44
+
+		  //Setze Zwischenwerte
+		  vZ[1] = vT[4][1]; // Z1 = T41
+		  vZ[2] = vT[4][3]; // Z2 = T43
+		  vZ[3] = vT[4][2]; // Z3 = T42
+		  vZ[4] = vT[4][4]; // Z4 = T44
+	  }
+	  
+	  //Runde 9, Ausgabetransformation
+	  vC[1] = calcMultiplikationZ(vZ[1], vK[9][1]);	//Z1 MultZ K1[9]	> T51
+	  vC[2] = calcAdditionMod216(vZ[2], vK[9][2]);	//Z2 Add216 K2[9]	> T52
+	  vC[3] = calcAdditionMod216(vZ[3], vK[9][3]);	//Z3 Add216 K3[9]	> T53
+	  vC[4] = calcMultiplikationZ(vZ[4], vK[9][4]);	//Z4 MultZ K4[9]	> T54
+	  
+	  //Rückgabe
+	  return vC;
   }
 
 }
