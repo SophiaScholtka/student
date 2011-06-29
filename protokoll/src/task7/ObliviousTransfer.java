62fe158d4a76a6775e26bf5f4a48c3dcb0990034
Saskia <saskia@pc>
2011-06-29 11:40:03 +0200

    Debugausgaben erweitert

--- protokoll/src/task7/ObliviousTransfer.java
+++ protokoll/src/task7/ObliviousTransfer.java
@@ -131,6 +131,12 @@ public final class ObliviousTransfer implements Protocol {
 		Com.sendTo(1,send0.toString(RADIX_SEND_)); // send0
 		Com.sendTo(1,send1.toString(RADIX_SEND_)); // send1
 		Com.sendTo(1, sbig.toString(RADIX_SEND_)); // s
+		if (DEBUG) {
+			System.out.println("DDD| (3)d-f Berechnete Werte");
+			System.out.println("DDD| \t s = " + sbig);
+			System.out.println("DDD| \t send0 = " + send0.toString(16));
+			System.out.println("DDD| \t send1 = " + send1.toString(16));
+		}
 		
 		//(4) nichts tun
 	}
@@ -174,6 +180,7 @@ public final class ObliviousTransfer implements Protocol {
 		Com.sendTo(0, q.toString(RADIX_SEND_));
 		if (DEBUG) {
 			System.out.println("DDD| (2) B sendet an A:");
+			System.out.println("DDD| \t r = " + r);
 			System.out.println("DDD| \t q = " + q);
 		}
 		//(3)b Bob empfängt die Signaturen von k0' und k1' von Alice
@@ -193,6 +200,12 @@ public final class ObliviousTransfer implements Protocol {
 		BigInteger rec1 = new BigInteger(sReceive, RADIX_SEND_);
 		sReceive = Com.receive();
 		BigInteger s = new BigInteger(sReceive, RADIX_SEND_);
+		if(DEBUG) {
+			System.out.println("DDD| (3)d Bob hat empfangen");
+			System.out.println("DDD| \t rec0 (send0) = " + rec0.toString(16));
+			System.out.println("DDD| \t rec1 (send1) = " + rec1.toString(16));
+			System.out.println("DDD| \t s = " + s.toString(16));
+		}
 		
 		//(4)a Bob berechnet M_(s xor r)
 		BigInteger biR = new BigInteger(""+r,10);
