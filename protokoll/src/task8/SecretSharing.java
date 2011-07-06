a6fa022067bb5cbfef4571c395f7d5607e695232
Saskia <saskia@pc>
2011-07-06 13:00:27 +0200

    Debugausgabe Alice, was die Geheimnisse von Bob sind

--- protokoll/src/task8/SecretSharing.java
+++ protokoll/src/task8/SecretSharing.java
@@ -91,6 +91,9 @@ public final class SecretSharing implements Protocol {
 		sendSecrets(1,ssa);
 		// (SS3) Alice empfängt Geheimnisse
 		SecretWord[][] ssb = receiveSecrets(1,ssn.intValue());
+		if (DEBUG_SS) {
+			showSecrets("DDD| \t " , ssb, 36);
+		}
 		
 		// (SS3) Tausche y aus
 		// Solange weniger als m bits gesendet
@@ -625,12 +628,12 @@ public final class SecretSharing implements Protocol {
 		for (int i = 0 ; i < secrets.length ; i++) {
 			System.out.print(pre);
 			System.out.print(secrets[i][0].getSecret().toString(rad));
-			System.out.println("\t");
-			System.out.print("(" + secrets[i][0].getSecret().toString(rad) + ")");
-			System.out.println("\t\t");
+			System.out.print("\t");
+			System.out.print("(" + secrets[i][0].getGuessedSecret().toString(rad) + ")");
+			System.out.print("\t\t");
 			System.out.print(secrets[i][1].getSecret().toString(rad));
-			System.out.println("\t");
-			System.out.print("(" + secrets[i][1].getSecret().toString(rad) + ")");
+			System.out.print("\t");
+			System.out.print("(" + secrets[i][1].getGuessedSecret().toString(rad) + ")");
 			System.out.println();
 		}
 	}
